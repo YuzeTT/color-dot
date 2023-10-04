1646650e793d415d1fe03d926efd2a05da574b51
YuzeTT <ziv1024@163.com>
2023-10-04 17:31:30 +0800

    add: many colors

---
+++ data/colors/ant-design.ts
@@ -0,0 +1,66 @@
+export default {
+  name: 'Ant Design',
+  url: 'https://ant.design/',
+  colors: [
+    {
+      name: 'Dust Red / 薄暮',
+      main_color: '#f5222d',
+      color_list: ['#fff1f0','#ffccc7','#ffa39e','#ff7875','#ff4d4f','#f5222d','#cf1322','#a8071a','#820014','#5c0011',]
+    },
+    {
+      name: 'Volcano / 火山',
+      main_color: '#fa541c',
+      color_list: ['#fff2e8','#ffd8bf','#ffbb96','#ff9c6e','#ff7a45','#fa541c','#d4380d','#ad2102','#871400','#610b00',]
+    },
+    {
+      name: 'Sunset Orange / 日暮',
+      main_color: '#fa8c16',
+      color_list: ['#fff7e6','#ffe7ba','#ffd591','#ffc069','#ffa940','#fa8c16','#d46b08','#ad4e00','#873800','#612500',]
+    },
+    {
+      name: 'Calendula Gold / 金盏花',
+      main_color: '#faad14',
+      color_list: ['#fffbe6','#fff1b8','#ffe58f','#ffd666','#ffc53d','#faad14','#d48806','#ad6800','#874d00','#613400',]
+    },
+    {
+      name: 'Sunrise Yellow / 日出',
+      main_color: '#fadb14',
+      color_list: ['#feffe6','#ffffb8','#fffb8f','#fff566','#ffec3d','#fadb14','#d4b106','#ad8b00','#876800','#614700',]
+    },
+    {
+      name: 'Lime / 青柠',
+      main_color: '#a0d911',
+      color_list: ['#fcffe6','#f4ffb8','#eaff8f','#d3f261','#bae637','#a0d911','#7cb305','#5b8c00','#3f6600','#254000',]
+    },
+    {
+      name: 'Polar Green / 极光绿',
+      main_color: '#52c41a',
+      color_list: ['#f6ffed','#d9f7be','#b7eb8f','#95de64','#73d13d','#52c41a','#389e0d','#237804','#135200','#092b00',]
+    },
+    {
+      name: 'Cyan / 明青',
+      main_color: '#13c2c2',
+      color_list: ['#e6fffb','#b5f5ec','#87e8de','#5cdbd3','#36cfc9','#13c2c2','#08979c','#006d75','#00474f','#002329',]
+    },
+    {
+      name: 'Daybreak Blue / 拂晓蓝',
+      main_color: '#1677ff',
+      color_list: ['#e6f4ff','#bae0ff','#91caff','#69b1ff','#4096ff','#1677ff','#0958d9','#003eb3','#002c8c','#001d66',]
+    },
+    {
+      name: 'Geek Blue / 极客蓝',
+      main_color: '#2f54eb',
+      color_list: ['#f0f5ff','#d6e4ff','#adc6ff','#85a5ff','#597ef7','#2f54eb','#1d39c4','#10239e','#061178','#030852',]
+    },
+    {
+      name: 'Golden Purple / 酱紫',
+      main_color: '#722ed1',
+      color_list: ['#f9f0ff','#efdbff','#d3adf7','#b37feb','#9254de','#722ed1','#531dab','#391085','#22075e','#120338',]
+    },
+    {
+      name: 'Magenta / 法式洋红',
+      main_color: '#eb2f96',
+      color_list: ['#fff0f6','#ffd6e7','#ffadd2','#ff85c0','#f759ab','#eb2f96','#c41d7f','#9e1068','#780650','#520339',]
+    },
+  ]
+}
---
+++ data/colors/element-plus.ts
@@ -0,0 +1,51 @@
+export default {
+  name: 'Element Plus',
+  url: 'http://element-plus.org/',
+  colors: [
+    {
+      name: 'Primary Color',
+      main_color: '#409EFF',
+      color_list: ['#337ECC', '#79BBFF', '#A0CFFF', '#C6E2FF', '#D9ECFF', '#ECF5FF']
+    },
+    {
+      name: 'Success',
+      main_color: '#67C23A',
+      color_list: ['#529B2E', '#95D475', '#B3E19D', '#D1EDC4', '#E1F3D8', '#F0F9EB']
+    },
+    {
+      name: 'Warning',
+      main_color: '#E6A23C',
+      color_list: ['#B88230', '#EEBE77', '#F3D19E', '#F8E3C5', '#FAECD8', '#FDF6EC']
+    },
+    {
+      name: 'Danger',
+      main_color: '#F56C6C',
+      color_list: ['#C45656', '#F89898', '#FAB6B6', '#FCD3D3', '#FDE2E2', '#FEF0F0']
+    },
+    {
+      name: 'Info',
+      main_color: '#909399',
+      color_list: ['#73767A', '#B1B3B8', '#C8C9CC', '#DEDFE0', '#E9E9EB', '#F4F4F5']
+    },
+    // {
+    //   name: 'Background Colors',
+    //   main_color: '#FFFFFF',
+    //   color_list: ['#F2F3F5', '#FFFFFF']
+    // },
+    {
+      name: 'Text Colors',
+      main_color: '#303133',
+      color_list: ['#606266', '#909399', '#A8ABB2', '#C0C4CC']
+    },
+    // {
+    //   name: 'Border Colors',
+    //   main_color: '#DCDFE6',
+    //   color_list: ['#E4E7ED', '#EBEEF5', '#F2F6FC', '#D4D7DE', '#CDD0D6']
+    // },
+    // {
+    //   name: 'Fill Colors',
+    //   main_color: '#F0F2F5',
+    //   color_list: ['#F5F7FA', '#FAFAFA', '#FAFCFF', '#EBEDF0', '#E6E8EB', '#FFFFFF']
+    // }
+  ]
+}
